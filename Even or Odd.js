fef5420f63bc0faf381594fe78f06ed2d6b73efb
Palma <1hernandezj3@gmail.com>
2021-11-02 13:16:41 -0400

    My first code war challange, created a function that returns "Even" or "Odd" passed on the number input

---
+++ Even or Odd.js
@@ -0,0 +1,4 @@
+//  Create a function that takes an integer as an argument
+//  and returns "Even" for even numbers or "Odd" for odd numbers.
+
+const even_or_odd = (number) => (number % 2 === 0 ? 'Even' : 'Odd');
