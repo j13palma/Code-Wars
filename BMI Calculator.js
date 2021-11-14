85c0fb16a6360b4958afcabbe8bef72f959fbab8
Palma <1hernandezj3@gmail.com>
2021-11-14 03:27:14 -0500

    Rounded the BMI return

--- BMI Calculator.js
+++ BMI Calculator.js
@@ -6,12 +6,11 @@ BMI = weight(kg) / height^2(m)
 
 //Create your function below this line.
 function bmiCalculator(weight, height) {
-  return weight / Math.pow(height, 2);
+  return Math.round(weight / Math.pow(height, 2));
 }
 //The first parameter should be the weight and the second should be the height.
 
 var bmi = bmiCalculator(65, 1.8);
-console.log();
 
 /* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
 
