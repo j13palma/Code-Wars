61fd910813b5903e0ab7aca4ea2ee914bca60ca8
Palma <1hernandezj3@gmail.com>
2021-11-14 03:20:43 -0500

    Created a function that calculates the BMI of a person with given weight and height in kg and meters

---
+++ BMI Calculator.js
@@ -0,0 +1,22 @@
+/*
+Create a BMI calculator using JavaScript functions.
+
+BMI = weight(kg) / height^2(m)
+*/
+
+//Create your function below this line.
+function bmiCalculator(weight, height) {
+  return weight / Math.pow(height, 2);
+}
+//The first parameter should be the weight and the second should be the height.
+
+var bmi = bmiCalculator(65, 1.8);
+console.log();
+
+/* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
+
+var bmi = bmiCalculator(65, 1.8); 
+
+bmi should equal 20 when it's rounded to the nearest whole number.
+
+*/
