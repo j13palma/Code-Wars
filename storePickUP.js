4a3f56e842f07429b06410ac3e4254598d06d725
Palma <1hernandezj3@gmail.com>
2021-11-14 02:42:20 -0500

    Created a function that simulates a robot going to the store to go pick up some milk. Function takes in money and finds out how many bottles of milk can be purchased

---
+++ storePickUP.js
@@ -0,0 +1,27 @@
+//  Challenge from webDev Bootcamp
+
+function getMilk(money) {
+  let purchase = Math.floor(money / 1.5);
+
+  console.log('leaveHouse');
+  console.log('moveRight');
+  console.log('moveRight');
+  console.log('moveUp');
+  console.log('moveUp');
+  console.log('moveUp');
+  console.log('moveUp');
+  console.log('moveRight');
+  console.log('moveRight');
+  console.log(`buy ${purchase} bottles of milk`);
+  console.log('moveLeft');
+  console.log('moveLeft');
+  console.log('moveDown');
+  console.log('moveDown');
+  console.log('moveDown');
+  console.log('moveDown');
+  console.log('moveLeft');
+  console.log('moveLeft');
+  console.log('enterHouse');
+}
+
+getMilk(16);
