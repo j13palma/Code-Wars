9625772720138f1cea3e634830bfd863d552d036
Palma <1hernandezj3@gmail.com>
2022-02-17 23:01:11 -0500

    "Create function that gets values from an array and adds them if they are positive"

---
+++ Find positive number not in array.js
@@ -0,0 +1,33 @@
+// This is a demo task.
+
+// Write a function:
+
+// function solution(A);
+
+// that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+// For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+// Given A = [1, 2, 3], the function should return 4.
+
+// Given A = [−1, −3], the function should return 1.
+
+// Write an efficient algorithm for the following assumptions:
+
+// N is an integer within the range [1..100,000];
+// each element of array A is an integer within the range [−1,000,000..1,000,000].
+
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(A) {
+  // write your code in JavaScript (Node.js 8.9.4)
+  let lowest = 1;
+  A = A.sort((a, b) => a - b);
+  A.forEach((num) => {
+    if (num === lowest) {
+      lowest++;
+    }
+  });
+  return lowest;
+}
