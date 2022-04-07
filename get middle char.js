d6f207205bea513aa28402e9bae0601c2c33dd25
Palma <1hernandezj3@gmail.com>
2022-04-07 18:10:41 -0400

    "Create function that gets middle char(s) in str"

---
+++ get middle char.js
@@ -0,0 +1,8 @@
+function getMiddle(s) {
+  //Code goes here!
+  if (s.length % 2 === 0) {
+    return s[s.length / 2 - 1] + s[s.length / 2];
+  } else {
+    return s[Math.floor(s.length / 2)];
+  }
+}
